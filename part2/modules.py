ebdde7e01e2794c7794869f0affb2c17ba6b5b04
Luis Piloto <luis.r.piloto@gmail.com>
2022-02-25 23:13:36 +0000

    Implement NALU and NAC modules.
    These modules are from the Neural Arithmetic Logic Units paper.
    
    - NAC is for addition.
    - NALU is for addition OR multiplication.

---
+++ part2/modules.py
@@ -0,0 +1,45 @@
+from typing import Optional
+import jax
+import jax.numpy as jnp
+import haiku as hk
+
+
+class NAC(hk.Module):
+  """Implementation of Neural accumulator to add input values together."""
+
+  def __init__(self, output_size: int, name: Optional[str] = None):
+    super().__init__(name=name)
+    self._output_size = output_size
+
+
+  def __call__(self, x: jnp.ndarray):
+    input_size = x.shape[-1]
+    w_hat = hk.get_parameter("w_hat", shape=[input_size, self._output_size], dtype=x.dtype,
+        init=hk.initializers.VarianceScaling())
+    m_hat = hk.get_parameter("m_hat", shape=[input_size, self._output_size], dtype=x.dtype,
+        init=hk.initializers.VarianceScaling())
+    w = jnp.multiply(jax.nn.tanh(w_hat), jax.nn.sigmoid(m_hat))
+    output = jnp.dot(x, w)
+    return output
+
+
+
+class NALU(hk.Module):
+  """Implementation of Neural Arithmetic Logic Unit to add or multiply input values together."""
+
+  def __init__(self, output_size: int, eps: float = 1e-9, name: Optional[str] = None):
+    super().__init__(name=name)
+    self._output_size = output_size
+    self._eps = eps
+    self._nac = NAC(output_size, name='nac')
+
+  def __call__(self, x: jnp.ndarray):
+    input_size = x.shape[-1]
+    big_g = hk.get_parameter("big_g", shape=[input_size, self._output_size], dtype=x.dtype,
+        init=hk.initializers.VarianceScaling())
+    g = jax.nn.sigmoid(jnp.dot(x, big_g))
+    a = self._nac(x)
+    m = jnp.exp(self._nac(jnp.log(jnp.abs(x) + self._eps)))
+    output = jnp.multiply(g, a) + jnp.multiply(1. - g, m)
+    return output
+
